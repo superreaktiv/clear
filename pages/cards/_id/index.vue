84379304ae5a5dbb930b53554bf64fceb9e39d54
superreaktiv <nepishet@gmail.com>
2018-03-29 16:03:07 +0400

    first commit

---
+++ pages/cards/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="single-card">
+      <h1>{{ $route.params.id }} </h1>
+      <div>
+          <img src="" alt="">
+      </div>
+      <p>The card description</p>
+  </section>
+</template>
+
+<style scoped>
+.single-card {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+</style>
+
---
+++ pages/cards/index.vue
@@ -0,0 +1,34 @@
+
+<template> 
+    <section class="cards">
+        <Card 
+            thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9uRAbh6tgB-L03B2kvBZ465g-oGZu4xiHpN5RK4_Y0TBxFZex"
+            title="Title"
+            id="1"
+            previewText="1244566" />
+        <Card 
+            thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUNv-XYXJi3qS0kvVaDPdLKi8St5ThoVJhAz7fOwHAT420MRA7"
+            title="Title"
+            id="2"
+            previewText="1244566" />    
+        
+    </section>
+</template>
+<script>
+import Card from '@/components/Card'
+export default {
+  components: {
+      Card
+  }
+}
+</script>
+
+<style scoped>
+.cards{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+</style>
